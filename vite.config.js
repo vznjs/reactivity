d47f75742c7ec0853d6ce223b7ea7300ca30ef8d
Maciej Kwaśniak <kmaciek17@gmail.com>
2022-01-03 20:59:52 +0100

    Minify

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 module.exports = defineConfig({
   build: {
     target: "es2020",
-    minify: false,
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       formats: ["cjs", "es"],
